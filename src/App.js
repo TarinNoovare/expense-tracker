79041b28671a1125d47b7f88dcb76a8d7b78a48b
Tarin Noovare <tarin.no@tnis.com>
2022-03-27 12:33:55 +0700

    Add ledger section by date of occurence

--- src/App.js
+++ src/App.js
@@ -6,7 +6,22 @@ import Ledger from "./components/IncomeExpenseSummary/Ledger";
 
 import DummyProfileImage from "./resources/myself.jpeg";
 
+const ledgerDict = {
+  "2022-03-27": [{ id: 1, type: "expense", title: "Hat", price: 280 }],
+  "2021-03-28": [
+    { id: 1, type: "expense", title: "Shirt", price: 200 },
+    { id: 2, type: "expense", title: "T-Shirt", price: 300 },
+  ],
+  "2021-03-31": [
+    { id: 1, type: "income", title: "Salary", price: 18000 },
+    { id: 2, type: "expense", title: "Nintendo Game", price: 2000 },
+    { id: 3, type: "expense", title: "Hand Controller", price: 5000 },
+    { id: 4, type: "expense", title: "Babayaka", price: 1000 },
+  ],
+};
+
 function App() {
+  
   return (
     <div className="App">
       <header className="App__wallet-header">
@@ -14,7 +29,7 @@ function App() {
         <IncomeExpenseSummary />
       </header>
       <main className="App__wallet-ledger">
-        <Ledger />
+        <Ledger ledgerDict={ledgerDict}/>
       </main>
     </div>
   );
--- src/components/IncomeExpenseSummary/Ledger.js
+++ src/components/IncomeExpenseSummary/Ledger.js
@@ -1,9 +1,79 @@
 import "./Ledger.css";
+import PriceFormatter from "../Formatter/PriceFormatter";
 
-const helloWorld = "hello";
+const DAYS_OF_WEEK = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const MONTHS_OF_YEAR = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-function Ledger() {
-  return <div>{helloWorld}</div>;
+const zeroPad = (num, places) => String(num).padStart(places, "0");
+
+function Ledger(props) {
+  const ledgerDict = props.ledgerDict;
+
+  const ledgerComponents = [];
+
+  for (const [datetimeKey, ledgerItems] of Object.entries(ledgerDict)) {
+    const dateTimeData = new Date(datetimeKey);
+
+    const dateData = zeroPad(dateTimeData.getDate(), 2);
+    const dayOfWeek = DAYS_OF_WEEK[dateTimeData.getDay()];
+    const monthOfYear = MONTHS_OF_YEAR[dateTimeData.getMonth()];
+    const yearData = dateTimeData.getFullYear();
+
+    ledgerComponents.push(
+      <div className="ledger-container" key={datetimeKey}>
+        <div className="ledger-container__datetime">
+          <div className="ledger-container__datetime-date">{dateData}</div>
+          <div className="ledger-container__datetime-day">{dayOfWeek}</div>
+          <div className="ledger-container__datetime-month-year">
+            {monthOfYear} {yearData}
+          </div>
+        </div>
+        <div className="ledger-container__ledger-items">
+          {ledgerItems.map((item) => {
+            return (
+              <div
+                className="ledger-container__item"
+                key={datetimeKey + "-" + item.id}
+              >
+                <div className="ledger-container__item-title">{item.title}</div>
+                <div className="ledger-container__item-price">
+                  <PriceFormatter
+                    value={
+                      item.type === "expense" ? item.price * -1 : item.price
+                    }
+                    isSignPrefix={true}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+
+  return <div className="ledger">{ledgerComponents}</div>;
 }
 
 export default Ledger;
