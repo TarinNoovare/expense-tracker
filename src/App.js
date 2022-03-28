765d1263ddda8324bcc6dbf7aad256d7f97f9d9e
Tarin Noovare <tarin.no@tnis.com>
2022-03-28 08:11:45 +0700

    Change from CSS global file to styled-component based CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import "./App.css";
-
 import TotalBalance from "./components/Wallet/TotalBalance";
 import IncomeExpenseSummary from "./components/IncomeExpenseSummary/IncomeExpenseSummary";
 import Ledger from "./components/IncomeExpenseSummary/Ledger";
+import styled from "styled-components";
 
+// File for testing
 import DummyProfileImage from "./resources/myself.jpeg";
-
 import { ledgerDict } from "./resources/ledgerExample";
+
+const AppHeaderStyled = styled.header`
+  top: 0;
+  position: sticky;
+`;
+
+const AppStyled = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 0.8em;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App__wallet-header">
+    <AppStyled>
+      <AppHeaderStyled>
         <TotalBalance profileImage={DummyProfileImage} />
         <IncomeExpenseSummary />
-      </header>
-      <main className="App__wallet-ledger">
+      </AppHeaderStyled>
+      <main>
         <Ledger ledgerDict={ledgerDict} />
       </main>
-    </div>
+    </AppStyled>
   );
 }
 
--- src/components/IncomeExpenseSummary/IncomeExpenseSummary.js
+++ src/components/IncomeExpenseSummary/IncomeExpenseSummary.js
@@ -1,43 +1,78 @@
-import "./IncomeExpenseSummary.css";
 import PriceFormatter from "../Formatter/PriceFormatter";
+import styled from "styled-components";
+
+const IncomeExpenseSummaryStyled = styled.div`
+  width: 100%;
+  height: 70px;
+  background-color: rgb(255, 255, 255);
+  border-top: 0.5px solid rgb(87, 87, 87);
+  border-bottom: 0.5px solid rgb(87, 87, 87);
+  font-size: 0.8em;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+
+  & .income {
+    margin-left: 2%;
+    margin-right: 2%;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  & .expense {
+    margin-left: 2%;
+    margin-right: 2%;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  & .difference {
+    margin-left: 2%;
+    margin-right: 2%;
+    display: flex;
+    justify-content: flex-end;
+    font-weight: 700;
+  }
+
+  & .value {
+    text-align: end;
+    width: 40%;
+  }
+
+  & .difference .value {
+    padding-top: 0.2em;
+    border-top: 0.5px solid rgb(139, 139, 139);
+  }
+`;
 
 function IncomeExpenseSummary() {
-
   const income = 14000.04;
-  const expense = -5000;
+  const expense = 5000;
 
-  const differenceInOut = income + expense;
+  const differenceInOut = income - expense;
 
   return (
-    <div className="summary-container">
-      <div className="summary-container__income">
+    <IncomeExpenseSummaryStyled>
+      <div className="income">
         <div>Income</div>
-        <div className="summary-container__value">
-          <PriceFormatter
-            value={income}
-            isSignPrefix={true}
-          />
-        </div>
-      </div>
-      <div className="summary-container__expense">
-        <div>Expense</div>
-        <div className="summary-container__value">
-          <PriceFormatter
-            value={expense}
-            isSignPrefix={true}
-          />
+        <div className="value">
+          <PriceFormatter value={income} isSignPrefix={true} />
         </div>
       </div>
 
-      <div className="summary-container__difference">
-        <div className="summary-container__value">
-        <PriceFormatter
-            value={differenceInOut}
-            isSignPrefix={true}
-          />
+      <div className="expense">
+        <div>Expense</div>
+        <div className="value">
+          <PriceFormatter value={-expense} isSignPrefix={true} />
         </div>
       </div>
-    </div>
+
+      <div className="difference">
+        <div className="value">
+          <PriceFormatter value={differenceInOut} isSignPrefix={true} />
+        </div>
+      </div>
+    </IncomeExpenseSummaryStyled>
   );
 }
 
